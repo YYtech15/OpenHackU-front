0581ce8c8b67c15310f34c43b93b466bc2be9daa
YYtech15 <midori1140flower@gmail.com>
2023-02-21 11:19:45 +0900

    写真撮影機能実装

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -1,11 +1,32 @@
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:myapp/view/signIn/login_page.dart';
+import 'package:myapp/view/user/camera/take_pictures.dart';
 
-void main(){
-  runApp(MyApp());
+Future<void> main() async {
+  // main 関数内で非同期処理を呼び出すための設定
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // デバイスで使用可能なカメラのリストを取得
+  final cameras = await availableCameras();
+
+  // 利用可能なカメラのリストから特定のカメラを取得
+  final firstCamera = cameras.first;
+
+  // 取得できているか確認
+  // print(firstCamera);
+
+  runApp(MyApp(camera: firstCamera));
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({
+    Key? key,
+    required this.camera,
+  }) : super(key: key);
+
+  final CameraDescription camera;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -14,7 +35,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.deepOrange,
       ),
-      home: const LoginPage(),
+      home: TakePictureScreen(camera: camera),
     );
   }
 }
